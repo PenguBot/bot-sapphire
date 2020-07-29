dc80e99dfa0f6f1bc253aa67da8522fbb4dd8487
Skillz <gankstarsleague@gmail.com>
2020-07-29 13:01:44 -0400

    add secret.json file and remoe mongos stuff

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,4 @@
 import Bot from './Bot';
-import config from './configs/config.json';
-
-if (config.settings.db === 2) {
-    try {
-        // @ts-ignore
-        const mongoose = require('mongoose'); // eslint-disable-line
-        mongoose.connect('mongodb://localhost/AxonCoreDB', {
-            useCreateIndex: true,
-            autoReconnect: true,
-        } )
-            .then( () => {
-                Bot.logger.notice('Connected to AxonCore DataBase.');
-            } )
-            .catch( (err: Error) => {
-                Bot.logger.fatal(`Could NOT connect to AxonCore DataBase.\n${err.stack}`);
-            } );
-    } catch (e) {
-        Bot.logger.fatal(`Could NOT connect to AxonCore DataBase.\n${e.stack}`);
-    }
-}
 
 Bot.start();
 
