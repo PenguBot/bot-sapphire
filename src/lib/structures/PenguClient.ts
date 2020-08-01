17d6a9e5113b36ed9f9a20cdf290ad555537f4c2
Skillz <gankstarsleague@gmail.com>
2020-08-01 16:02:48 -0400

    remove prisma imports

--- src/lib/structures/PenguClient.ts
+++ src/lib/structures/PenguClient.ts
@@ -1,13 +1,9 @@
 import { AnteiClient, AnteiClientOptions } from "@pengubot/antei";
-import { PrismaClient } from "@prisma/client";
 
 export class PenguClient extends AnteiClient {
 
-    public prisma: PrismaClient;
 
     public constructor(options?: AnteiClientOptions) {
         super({ ...options });
-
-        this.prisma = new PrismaClient();
     }
 }
