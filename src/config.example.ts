34ca73b228ddb20137bd6b452db6be3e60a957fd
QuantumlyTangled <ethoarx@gmail.com>
2020-08-01 22:51:30 +0200

    feat: typeorm config

--- src/config.example.ts
+++ src/config.example.ts
@@ -2,11 +2,20 @@
 
 import { AnteiClientOptions } from "@pengubot/antei";
 
+
+export const DEV = "DEV" in process.env ? process.env.DEV === "true" : !("PM2_HOME" in process.env);
+
 export const TOKEN = "";
 export const PRODUCTION = false;
 export const OWNERS = [""];
 export const PREFIX = "pd!";
 export const SHARDS = "auto";
+
+export const PGSQL_DATABASE_NAME = "";
+export const PGSQL_DATABASE_PASSWORD = "";
+export const PGSQL_DATABASE_USER = "";
+export const PGSQL_DATABASE_PORT = 5432;
+export const PGSQL_DATABASE_HOST = "localhost";
 
 export const CLIENTOPTIONS: AnteiClientOptions = {
     shards: SHARDS,
