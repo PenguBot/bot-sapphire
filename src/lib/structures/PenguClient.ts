d44b306432d1a997270eba30544c9abfbcdc57db
Nejc Drobnic <ethoarx@gmail.com>
2020-08-01 22:28:29 +0200

    feat: remove prisma (#16)

--- src/lib/structures/PenguClient.ts
+++ src/lib/structures/PenguClient.ts
@@ -1,13 +1,8 @@
 import { AnteiClient, AnteiClientOptions } from "@pengubot/antei";
-import { PrismaClient } from "@prisma/client";
 
 export class PenguClient extends AnteiClient {
 
-    public prisma: PrismaClient;
-
     public constructor(options?: AnteiClientOptions) {
         super({ ...options });
-
-        this.prisma = new PrismaClient();
     }
 }
