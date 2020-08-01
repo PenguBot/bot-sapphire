efb0c039577ee425e759b6dc359c7df0248ad638
Skillz <gankstarsleague@gmail.com>
2020-08-01 15:58:27 -0400

    remove prisma

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { PrismaClient } from "@prisma/client";
-// Exported so we can use this in any file we want.
-export const prisma = new PrismaClient();
-
 export const main = () => "this builds and pushes";
