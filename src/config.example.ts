8ae59f4681445cdf65c2e2c1b7670bf20894f998
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-08-01 14:06:10 +0530

    feat: base of the bot

---
+++ src/config.example.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+export const TOKEN = "";
+export const PRODUCTION = false;
+export const OWNERS = [""];
+export const PREFIX = "pd!";
+export const SHARDS = "auto";
