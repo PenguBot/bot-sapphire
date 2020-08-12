97044d97c53b67d066a3fdbbb6c5121780df19f8
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-08-12 19:12:57 +0530

    feat: i18n base

--- src/lib/structures/PenguClient.ts
+++ src/lib/structures/PenguClient.ts
@@ -1,8 +1,12 @@
 import { KlasaClient, KlasaClientOptions } from "klasa";
+import { LanguageHandler } from "./LanguageHandler";
 
 export class PenguClient extends KlasaClient {
+    i18n: LanguageHandler;
 
     public constructor(options?: KlasaClientOptions) {
         super({ ...options });
+
+		this.i18n = new LanguageHandler(this);
     }
 }
