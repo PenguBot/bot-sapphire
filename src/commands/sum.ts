69d0c41dc3b73c889618a0592504fe51743b6b61
QuantumlyTangled <ethoarx@gmail.com>
2020-08-28 12:17:32 +0200

    fix(types): augments

--- src/commands/sum.ts
+++ src/commands/sum.ts
@@ -3,7 +3,7 @@ import { Message } from "discord.js";
 
 export class SumCommand extends Command {
     public async run(message: Message, args: Args) {
-        const ints = await args.repeat("number" as "integer");
+        const ints = await args.repeat("number");
         return message.channel.send(`Sum is ${ints.reduce((a: number, b: number) => a + b, 0)}`);
     }
 }
--- src/types/Augments.d.ts
+++ src/types/Augments.d.ts
@@ -3,13 +3,6 @@ import { CacheManager } from "@lib/structures/CacheManager";
 import { LanguageHandler } from "@lib/structures/LanguageHandler";
 import { Redis as IRedis } from "ioredis";
 
-/* declare module "@sapphire/framework/dist/lib/utils/Args" {
-    interface ArgType {
-        float: number;
-        pNumber: number;
-    }
-} */
-
 declare module "discord.js" {
     interface Client {
         readonly redis: IRedis;
@@ -21,5 +14,12 @@ declare module "discord.js" {
         send(content: StringResolvable, options?: MessageOptions): Promise<Message>;
         sendLocale(key: string, args?: Record<string, unknown>, options?: MessageOptions): Promise<Message>;
         translate(key: string, args?: Record<string, unknown>): Promise<string>;
+    }
+}
+
+declare module "@sapphire/framework" {
+    interface ArgType {
+        float: number;
+        number: number;
     }
 }
