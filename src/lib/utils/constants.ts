7aaa2103dbde553ae3b95fc230d086365d6eb090
QuantumlyTangled <ethoarx@gmail.com>
2020-09-13 14:39:44 +0200

    feat: list formatting

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { Emojis } from "@utils/Enums";
+import { Emojis, PenguFormatters } from "@utils/Enums";
+import { list } from "@utils/languageFunctions";
 import { ClientOptions } from "discord.js";
+import i18next from "i18next";
 
 export const CLIENT_OPTIONS: ClientOptions = {
     i18n: {
@@ -10,6 +12,18 @@ export const CLIENT_OPTIONS: ClientOptions = {
                     pCross: Emojis.PenguCross,
                     pCheck: Emojis.PenguCheck,
                     pLoading: Emojis.PenguLoading
+                },
+                format: (value: unknown, format?: string, lng?: string) => {
+                    switch (format as PenguFormatters) {
+                        case PenguFormatters.AndList: {
+                            return list(value as string[], i18next.t("global:and", { lng }));
+                        }
+                        case PenguFormatters.OrList: {
+                            return list(value as string[], i18next.t("global:or", { lng }));
+                        }
+                        default:
+                            return value as string;
+                    }
                 }
             }
         }
